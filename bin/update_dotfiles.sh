7583298e52cf577530f6b41ada0d2f0bef58e423
sinarf <sinarf@sinarf.org>
2021-06-26 09:51:36 +0200

    Adding a check of DOT_FILES variable existence.

--- bin/update_dotfiles.sh
+++ bin/update_dotfiles.sh
@@ -4,7 +4,16 @@
 
 set -e  # stop on first error
 
-cd $DOTFILES
+if [ -z $DOT_FILES ]; then
+    echo "the DOTFILES environement variable should be set up for this script to work"
+    exit 2
+fi
+
+echo "Moving to the dotfiles directory: $DOT_FILES"
+cd $DOT_FILES
+echo "pulling change..."
 git pull
+echo "Installing packages..."
 ./package_install
+echo "Linking dotfiles..."
 ./install
