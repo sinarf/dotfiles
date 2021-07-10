f95dd331efff70f2b415aa14241e1d068cd773e0
sinarf <sinarf@sinarf.org>
2021-07-10 21:02:42 +0200

    Better git prompt for zsh

--- config/shell/theme.zsh
+++ config/shell/theme.zsh
@@ -47,4 +47,9 @@ setopt prompt_subst
 RPROMPT=\$vcs_info_msg_0_
 zstyle ':vcs_info:git:*' formats '%F{green}(%b)%r%f'
 zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr ' *'
+zstyle ':vcs_info:*' stagedstr ' +'
+zstyle ':vcs_info:git:*' formats       '(%b%u%c)'
+zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)'
 
