cb09a34fc3ffdd82e9491a2155407726eb72321b
sinarf <sinarf@sinarf.org>
2021-07-24 18:40:33 +0200

    Correct completion with aliases.

--- all/.config/shell/completion.zsh
+++ all/.config/shell/completion.zsh
@@ -1,15 +1,21 @@
 # COMPLETION
-autoload -U compinit
+autoload -Uz compinit
 compinit
 
 #allow tab completion in the middle of a word
 setopt COMPLETE_IN_WORD
+# autocompletion of command line switches for aliases
+setopt COMPLETE_ALIASES
+
 ### completion system
 # load module for list-style selection
 zmodload zsh/complist
 
 # use the module above for autocomplete selection
-zstyle ':completion:*' menu yes select
+zstyle ':completion:*' menu select
+
+# complete sudo commnands
+zstyle ':completion::complete:*' gain-privileges 1
 
 # now we can define keybindings for complist module
 # you want to trigger search on autocomplete items
