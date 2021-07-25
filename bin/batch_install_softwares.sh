1ed043f894d41f7b8da63eb9844b343978338aed
sinarf <sinarf@sinarf.org>
2021-07-25 13:24:12 +0200

    update the batch_install_softwares script
    Now if no parameter is provided, the script propose to fuzzy find a
    package list

--- bin/batch_install_softwares.sh
+++ bin/batch_install_softwares.sh
@@ -1,10 +1,16 @@
 #/usr/bin/env bash
 # basic script to install package on my arch system using paru
 
-# TODO add parameter verification
-
-filename=$1
-
 set +e # stop on firt error
+
+softwares_dir=$DOT_FILES/softwares
+if [ -n ${1+x} ]; 
+then
+    cd $softwares_dir
+    filename=$(fzf --prompt="No file provided, please choose one: ")
+else
+    filename=$1
+fi
+
 softwares=$(cat $filename)
 paru -S $softwares
