8aab0516781b5b1eecaebed06fa5bf7c19c4a77d
sinarf <sinarf@sinarf.org>
2022-01-29 14:25:18 +0100

    Switch to wide screen if plugged

--- bin/switch_to_wide_screen.sh
+++ bin/switch_to_wide_screen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if xrandr | grep -qE '^DP1 connected' ; then
+if xrandr | grep -qE '^DP1 connected 3440x1440' ; then
   notify-send "wide screen detected, let's disable the internal screen"
   xrandr --output eDP1 --off
 else
