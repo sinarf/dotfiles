756e07bd4bc787dfa52bca7fe8a334c0096d2456
sinarf <sinarf@sinarf.org>
2022-02-01 08:38:03 +0100

    Remaps caps lock to escape on i3wm

--- bin/remap_capslock_to_escape.sh
+++ bin/remap_capslock_to_escape.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+#!/usr/bin/env bash
+#xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+setxkbmap -option caps:escape
 notify-send "Escape is remapped to CAPSLOCK"
