795b3cf037ce914de8f0ce6b1d08e1720efe256e
sinarf <sinarf@sinarf.org>
2022-02-01 09:41:47 +0100

    handle HDMI

--- bin/switch_to_wide_screen.sh
+++ bin/switch_to_wide_screen.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-if xrandr | grep -qE '^DP1 connected 3440x1440' ; then
+if xrandr | grep -qE '^HDMI1 connected 3440x1440' ; then
   notify-send "wide screen detected, let's disable the internal screen"
   xrandr --output eDP1 --off
+  xrandr --output HDMI1 --primary
+  # polybar needs to be relauch after this
+  sleep 5
+  ~/.config/polybar/launch.sh
+elif xrandr | grep -qE '^DP1 connected 3440x1440' ; then
+  notify-send "wide screen detected, let's disable the internal screen"
+  xrandr --output eDP1 --off
+  xrandr --output DP1 --primary
+  # polybar needs to be relauch after this
+  sleep 5
+  ~/.config/polybar/launch.sh
 else
   notify-send "wide screen not detected, let's do nothing"
 fi
