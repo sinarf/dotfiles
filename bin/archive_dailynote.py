3263cb2533aaa34405cce9fbbdf9931365a69d81
Michel Blavin <michel.blavin@myscript.com>
2022-02-03 11:06:40 +0100

    Archive from the file date

--- bin/archive_dailynote.py
+++ bin/archive_dailynote.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
 
 from datetime import date
-from datetime import timedelta
-import os
+from os import environ, path
 import shutil
+import logging as log
 
-home = os.environ["HOME"]
-dailynote = os.environ["TODAY_NOTE"]
-
-yesterday = date.today() - timedelta(days=1)
-
-archivednote = home + "/Sync/pkb/main/diary/" + str(yesterday) + ".md"
+home = environ["HOME"]
+dailynote = environ["TODAY_NOTE"]
 
 
 def archivenote():
-    # only archive if not already done
-    if os.path.exists(archivednote):
-        print(archivednote + " already exists")
+    """
+    Creates a note name with the date of the last modified date of the note.
+    Only if the file has not been modified today.
+    """
+
+    last_modified = date.fromtimestamp(path.getmtime(dailynote))
+    today = date.today()
+
+    archivednote = home + "/Sync/pkb/main/diary/" + str(last_modified) + ".md"
+
+    if last_modified == today:
+        log.info(f"{str(dailynote)} already modified today.")
+    elif path.exists(archivednote):
+        # only archive if not already done
+        log.info(f"{str(archivednote)} already exists")
     else:
-        print(f"Archiving note {archivenote}")
+        log.info(f"Archiving note {str(archivenote)}")
         shutil.copyfile(dailynote, archivednote)
 
 
 if __name__ == "__main__":
+    log.basicConfig(filename=f"{home}/admin.log", level=log.INFO)
     archivenote()
