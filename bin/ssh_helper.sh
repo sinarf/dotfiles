7646eb011386e29466603c17548ca9c75ee0f989
sinarf <sinarf@sinarf.org>
2023-02-16 11:57:15 +0100

    Handle the remote execution of a command

--- bin/ssh_helper.sh
+++ bin/ssh_helper.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 server=${1}
-
-if [ -z "$TMUX" ]; then
-  ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes ${server}
+if [[ $# -gt "1" ]]; then
+  ssh "$@"
+elif [ -z "${TMUX}" ]; then
+  ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes "${server}"
 else
   tmux new-window "ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes ${server}"
   short_name=$(echo "${server}" | cut -f1 -d'.')
