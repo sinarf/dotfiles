42edddc9c7fc788f4631cfd2aa08b4b6bedaa46e
sinarf <sinarf@sinarf.org>
2023-07-11 11:15:53 +0200

    Auto fetch in a popup

--- bin/project_helper.sh
+++ bin/project_helper.sh
@@ -4,10 +4,11 @@
 #
 project_directory="${HOME}/git"
 
-
 project=$(find "${project_directory}" -maxdepth 4 -type d -name .git -exec dirname {} \; | fzf)
-tmux new-window -c "${project}"
-{
-  tmux send-keys " git fetch -p" Enter
-}
-tmux rename-window "$(basename "${project}")"
+
+if [[ -n ${project} ]]; then
+  tmux new-window -c "${project}"
+  tmux rename-window "$(basename "${project}")"
+else 
+  echo "No project selected, doing nothing..."
+fi
