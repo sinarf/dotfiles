8e5f81819c2d826b825ce631a5099d7828859974
sinarf <sinarf@sinarf.org>
2023-07-21 19:08:01 +0200

    Add default directories to project helper

--- bin/project_helper.sh
+++ bin/project_helper.sh
@@ -4,8 +4,12 @@
 #
 project_directory="${HOME}/git"
 
-project=$(find "${project_directory}" -maxdepth 4 -type d -name .git -exec dirname {} \; | fzf)
+default_directories="${HOME}/.dotfiles
+${HOME}/.config/nvim
+"
+project_directories=$(find "${project_directory}" -maxdepth 4 -type d -name .git -exec dirname {} \;)
 
+project=$( echo "${default_directories}" "${project_directories}" | fzf)
 if [[ -n ${project} ]]; then
   tmux new-window -c "${project}"
   tmux rename-window "$(basename "${project}")"
