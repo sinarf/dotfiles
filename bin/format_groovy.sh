6810b1c991e782f6ec2463dc921626edc2db8ea7
sinarf <sinarf@sinarf.org>
2023-09-19 16:21:52 +0200

    Remove old script

--- bin/format_groovy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v npm-groovy-lint >/dev/null; then
-  echo "npm-groovy-lint not found, install it first"
-  exit 20
-fi
-
-npm-groovy-lint --config "${HOME}"/.config/groovylintrc.json --output /tmp/npm-groovy-lint.json --format "$1"
