5775a7ad54e65a6bc69f9a3c59fd5511d0617d8f
sinarf <sinarf@sinarf.org>
2023-10-16 16:58:01 +0200

    Use an array instead of multiline string

--- bin/project_helper.sh
+++ bin/project_helper.sh
@@ -2,14 +2,20 @@
 
 # @author sinarf
 #
-project_directory="${HOME}/git"
+project_directory="$HOME/git"
 
-default_directories="${HOME}/.dotfiles
-${HOME}/.config/nvim
-"
-project_directories=$(find "${project_directory}" -maxdepth 4 -type d -name .git -exec dirname {} \;)
+if [[ -d $project_directory ]]; then
+  mkdir -pv "$project_directory"
+fi
 
-project=$( echo "${default_directories}" "${project_directories}" | fzf)
+default_directories=(
+  "$HOME/.dotfiles"
+  "$HOME/.config/nvim"
+)
+
+project_directories=$(find "$project_directory" -maxdepth 4 -type d -name .git -exec dirname {} \;)
+
+project=$( echo "${default_directories[@]}" "$project_directories" | fzf)
 if [[ -n ${project} ]]; then
   tmux new-window -c "${project}"
   tmux rename-window "$(basename "${project}")"
