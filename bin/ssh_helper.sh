05362790716156bc60eca09c45f2aee52f52a709
sinarf <sinarf@sinarf.org>
2024-01-16 18:08:08 +0100

    combine 2 tmux command in one

--- bin/ssh_helper.sh
+++ bin/ssh_helper.sh
@@ -6,7 +6,6 @@ if [[ $# -gt "2" ]]; then
 elif [ -z "${TMUX}" ]; then
   ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes "${server}"
 else
-  tmux new-window "ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes ${server}"
   short_name=$(echo "${server}" | cut -f1 -d'.')
-  tmux rename-window "${short_name}"
+  tmux new-window -n "$short_name" "ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes ${server}" || sleep 10
 fi
