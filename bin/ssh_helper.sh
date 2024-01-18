aea1f6cf7130dfca1fa4b2dc9b59ad8db496e61f
sinarf <sinarf@sinarf.org>
2024-01-18 17:57:16 +0100

    Give time to see ssh errors

--- bin/ssh_helper.sh
+++ bin/ssh_helper.sh
@@ -7,5 +7,5 @@ elif [ -z "${TMUX}" ]; then
   ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes "${server}"
 else
   short_name=$(echo "${server}" | cut -f1 -d'.')
-  tmux new-window -n "$short_name" "ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes ${server}" || sleep 10
+  tmux new-window -n "$short_name" "ssh -o RemoteCommand='bash -o vi' -o RequestTTY=yes ${server} || sleep 10"
 fi
