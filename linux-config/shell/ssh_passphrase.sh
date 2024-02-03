554434ae90fbefc2b6dc856df2d318e69d886e77
sinarf <sinarf@sinarf.org>
2024-02-03 23:04:28 +0100

    ricing

--- linux-config/shell/ssh_passphrase.sh
+++ linux-config/shell/ssh_passphrase.sh
@@ -1,4 +1,4 @@
 ssh_key_path="$HOME/.ssh/id_ed25519"
-if [ -f "$ssh_key_path" ]; then
+if command -v keychain > /dev/null && [ -f "$ssh_key_path" ]; then
   eval "$(keychain --eval --agents ssh "$ssh_key_path")"
-fi
+fi 
